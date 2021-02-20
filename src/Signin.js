8d2f531d5db80b8cd031ea57bd03748924a0ca36
m-perez33 <kingleisure@gmail.com>
2021-02-20 09:55:06 -0500

    temp change

--- src/Signin.js
+++ src/Signin.js
@@ -166,9 +166,9 @@ handleKeyPress = (event)=>{
                 required
               />
             </div>
-            <div className="lh-copy mt3">
+            {/*<div className="lh-copy mt3">
               <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
-            </div>
+            </div>*/}
           </div>
         </main>
       </article>
