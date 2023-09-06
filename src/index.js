94f51f4d9c40804e15ed6b15613a3c3ac4c03074
m-perez33 <kingleisure@gmail.com>
2023-09-06 12:54:19 -0400

    change

--- src/index.js
+++ src/index.js
@@ -11,14 +11,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(<App />)
 
-/*ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);/*
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
