4e3d182a11e3fae70bb90c877b663f56b4bb1ba9
m-perez33 <kingleisure@gmail.com>
2023-09-06 12:40:38 -0400

    update index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(<App />)
+
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+);*.
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
